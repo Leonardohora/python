8a93c7104bfa2d6f1373212d8de2efaee02916bd
Leonardo luis <155635781+Leonardohora@users.noreply.github.com>
2024-11-05 09:34:14 -0300

    Add files via upload
    atualização 05/11/2024 gerador de senhas

--- gerador_senhas.py
+++ gerador_senhas.py
@@ -1,40 +1,52 @@
+#importação das bibliotecas usadas
 import tkinter as tk
 import random as rd
 import string as str
 
-
+#configuração da janela
 tela = tk.Tk()
-tela.geometry("600x480")
+tela.geometry("600x480+500+200") #"+500+200 é para definir a posição inicial da janela na tela."
 tela.title("Gerador de senhas")
+
 
 bvindas = tk.Label(tela, text="Boas vindas ao Gerador de senhas.", bg="gray")
 bvindas.place(x=20, y=10)
 
+#variaveís de controle, serve para saber se as opções estão marcadas ou não. 1 = sim/0 == não
 opcao1 = tk.IntVar()
 opcao2 = tk.IntVar()
 opcao3 = tk.IntVar()
 
+
+#função para gerar senha.
 def gerar_senha():
-    comprimento = int(senha_comprimento.get())    
-    letras = ''
     
-    if opcao1.get() == 1:
-        letras += str.punctuation
+    if not senha_comprimento.get().isnumeric():
+        campo_vazio.config(text="digite um numero válido")
         
-    if opcao2.get() == 1:
-        letras += str.digits
+    else:
+        comprimento = int(senha_comprimento.get())    
+        letras = ''
         
-    if opcao3.get() == 1:
-        letras += str.ascii_uppercase
-        
-    letras += str.ascii_lowercase
-    
-    senha = ''.join(rd.choice(letras) for l in range(comprimento))
-    mostrar_senha.config(text=senha)
-        
-    return gerar_senha
+        if comprimento >= 8 and comprimento <= 20:
+            if opcao1.get() == 1:
+                letras += str.punctuation
+                
+            if opcao2.get() == 1:
+                letras += str.digits
+                
+            if opcao3.get() == 1:
+                letras += str.ascii_uppercase
+                
+            letras += str.ascii_lowercase
+            
+            senha = ''.join(rd.choice(letras) for l in range(comprimento))
+            mostrar_senha.config(text=senha)
+            
+        else:
+            erro.config(text="Sua senha deverá que ter entre 8 a 20 caracters.")
 
-
+#função para copiar a senha
 def copiar():
     tela.clipboard_clear()
     tela.clipboard_append(mostrar_senha["text"])
@@ -55,14 +67,20 @@ chk_simbolo2.grid(pady=1)
 chk_simbolo3 = tk.Checkbutton(tela, text="Letras maisculas", variable=opcao3,command=opcao3)
 chk_simbolo3.grid(pady=1)
 
-gerar = tk.Button(tela, text="clique aqui", command=lambda: gerar_senha())
-gerar.place(x=20, y=60)
+campo_vazio = tk.Label(tela, text="")
+campo_vazio.place(x=410, y=25)
+
+erro = tk.Label(tela,text="")
+erro.place(x=20, y=40)
+
+botao_gerar = tk.Button(tela, text="clique aqui", command=lambda: gerar_senha())
+botao_gerar.place(x=20, y=60)
 
 mostrar_senha = tk.Label(tela, text="")
 mostrar_senha.place(x=20,y=90)
 
-copiar_senha = tk.Button(tela, text="Copiar senha", command=lambda:copiar())
-copiar_senha.place(x=15, y=120)
+botao_copiar_senha = tk.Button(tela, text="Copiar senha", command=lambda:copiar())
+botao_copiar_senha.place(x=15, y=120)
 
 
 tela.mainloop()
